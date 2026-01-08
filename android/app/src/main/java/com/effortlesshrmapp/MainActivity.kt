c6d07cfb081967c34e2aa21aa427cbc2bbfd06ed
Mohd Rafi <dotnetexpertdev@gmail.com>
2026-01-08 20:46:33 +0530

    launcher icon changes

--- android/app/src/main/java/com/effortlesshrmapp/MainActivity.kt
+++ android/app/src/main/java/com/effortlesshrmapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.arsteg.effortlesshrmapp
+package com.effortlesshrmapp
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/effortlesshrmapp/MainApplication.kt
+++ android/app/src/main/java/com/effortlesshrmapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.arsteg.effortlesshrmapp
+package com.effortlesshrmapp
 
 import android.app.Application
 import android.content.res.Configuration
