3c7a9775c3aeae8257d7de0c287c0d9f871c5ffa
Mohamad Rafi <dotnetexpertdev@gmail.com>
2026-07-29 15:07:30 +0530

    updated the namespace for publish the build

--- android/app/src/main/java/com/arsteg/effortlesshrm/app/MainActivity.kt
+++ android/app/src/main/java/com/arsteg/effortlesshrm/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.arsteg.effortlesshrmapp
+package com.arsteg.effortlesshrm.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/arsteg/effortlesshrm/app/MainApplication.kt
+++ android/app/src/main/java/com/arsteg/effortlesshrm/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.arsteg.effortlesshrmapp
+package com.arsteg.effortlesshrm.app
 
 import android.app.Application
 import android.content.res.Configuration
